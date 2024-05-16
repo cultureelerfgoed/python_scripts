7ecd921ec3caea7c8793f4e4065233f18fd6fc7e
Patrick Mout <p.mout@cultureelerfgoed.nl>
2024-05-16 09:15:32 +0200

    Add files via upload

--- reconcilen termen op AAT.py
+++ reconcilen termen op AAT.py
@@ -5,7 +5,7 @@ import xml.etree.ElementTree as ET
 import re
 from fuzzywuzzy import fuzz
 from operator import itemgetter
-
+from datetime import datetime
 
 # Functie om trefwoorden te reconciliëren met de Getty API op basis van AAT-zoekopdrachten
 def reconcile_AAT_keyword(keyword):
@@ -20,10 +20,7 @@ def reconcile_AAT_keyword(keyword):
                 page_link = re.search(r"\[(.+?)\]", child.text.split(',')[0]).group(1)
                 uri = "http://vocab.getty.edu/page/aat/" + page_link
                 score = fuzz.token_sort_ratio(keyword, name)
-                if score > 95:
-                    match = True
-                else:
-                    match = False
+                match = score > 95
                 resource = {
                     "uri": uri,
                     "name": name,
@@ -58,25 +55,41 @@ def reconcile_and_export_excel(input_file_path, output_file_path, column_name):
     new_workbook = openpyxl.Workbook()
     new_sheet = new_workbook.active
 
-    new_sheet.cell(row=1, column=1, value="Gereconcilieerde URI")
-    new_sheet.cell(row=1, column=2, value=column_name)
+    new_sheet.cell(row=1, column=1, value=column_name)
+    new_sheet.cell(row=1, column=2, value="Gereconcilieerde URI")
+    new_sheet.cell(row=1, column=3, value="Gereconcilieerde Naam")
 
     for row_num in range(2, max_row + 1):
         keyword = sheet.cell(row=row_num, column=column_index).value
+        new_sheet.cell(row=row_num, column=1, value=keyword)
         if keyword:
             reconciled_resources = reconcile_AAT_keyword(keyword)
             if reconciled_resources:
-                for index, resource in enumerate(reconciled_resources, start=2):
-                    new_sheet.cell(row=row_num, column=1, value=resource["uri"])
-                    new_sheet.cell(row=row_num, column=index, value=resource["name"])
+                new_sheet.cell(row=row_num, column=2, value=reconciled_resources[0]["uri"])
+                new_sheet.cell(row=row_num, column=3, value=reconciled_resources[0]["name"])
+            else:
+                new_sheet.cell(row=row_num, column=2, value="Geen match gevonden")
+                new_sheet.cell(row=row_num, column=3, value="Geen match gevonden")
+        else:
+            new_sheet.cell(row=row_num, column=2, value="Geen trefwoord")
+            new_sheet.cell(row=row_num, column=3, value="Geen trefwoord")
 
-    new_workbook.save(output_file_path)
+        # Voortgangsindicator
+        print(f"Verwerken van rij {row_num - 1} van {max_row - 1} ({(row_num - 1) / (max_row - 1) * 100:.2f}%)")
+
+    # Datum en tijd toevoegen aan de uitvoerbestandsnaam
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    sanitized_column_name = re.sub(r'\W+', '_', column_name)  # Replaces non-alphanumeric characters with underscores
+    output_file_path_with_timestamp = output_file_path.replace(".xlsx", f"_{sanitized_column_name}_{timestamp}.xlsx")
+
+    new_workbook.save(output_file_path_with_timestamp)
+    print(f"Bestand opgeslagen als: {output_file_path_with_timestamp}")
 
 # Pad naar het invoer- en uitvoerbestand
-input_excel_file_path = "/Users/patrickmout/Downloads/cht_procedureentechniek.xlsx"
-output_excel_file_path = "/Users/patrickmout/Downloads/cht_procedureentechniek_match_AAT.xlsx"
+input_excel_file_path = "/Users/patrickmout/Downloads/VGWW/Onderzoek-Van-Gogh-tbv-Van-Gogh-Worldwide_AAT.xlsx"
+output_excel_file_path = "/Users/patrickmout/Downloads/VGWW/analysemethode_match_AAT.xlsx"
 # Kolomnaam waarin trefwoorden worden opgeslagen
-column_name = "cht_concept"
+column_name = "trefwoord.inhoud 8"
 
 # Reconcilieer het Excel-bestand en exporteer naar een nieuw bestand
 reconcile_and_export_excel(input_excel_file_path, output_excel_file_path, column_name)
